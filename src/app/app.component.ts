f6a1bb94a511e9a56ced89e0d4dbe778f629075d
ADMIN\Admin <hocnguyen1109200@gmail.com>
2023-09-13 00:06:29 +0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { BehaviorSubject, tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  tap,
+  timer,
+  map,
+  take,
+  Observable,
+  finalize,
+  Subject,
+  switchMap,
+  startWith,
+} from 'rxjs';
 import { Parentcomponent } from 'src/components/parent.component';
 import { SessionStorage } from 'src/services/sersion.service';
 import { TestService } from 'src/services/test.service';
@@ -11,13 +23,35 @@ import { TestService } from 'src/services/test.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [Parentcomponent, ReactiveFormsModule, RouterOutlet, RouterLink],
+  imports: [
+    Parentcomponent,
+    ReactiveFormsModule,
+    RouterOutlet,
+    RouterLink,
+    AsyncPipe,
+    NgIf,
+  ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   private _fb = inject(FormBuilder);
   readonly coreForm = this._fb.nonNullable.group({});
   private _test = inject(TestService);
   private readonly _ss = inject(SessionStorage);
+  restart$ = new Subject<void>();
+
+  start = 10;
+  countdonwn$ = this.restart$.pipe(
+    startWith('init start'),
+    switchMap(() => {
+      return timer(100, 1000).pipe(
+        map((i) => this.start - i),
+        take(this.start + 1),
+        finalize(() => {
+          this.start = 10;
+        })
+      );
+    })
+  );
 
   title = 'ngx-bootrap';
   config = [
@@ -60,4 +94,10 @@ export class AppComponent {
   getItem() {
     const a = this._ss.getItem('test');
   }
+
+  restart() {
+    this.restart$.next();
+  }
+
+  ngOnInit(): void {}
 }
